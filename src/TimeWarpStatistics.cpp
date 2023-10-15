e239b2cac89061e1471e47742740a23fb63186fe
gourav-shinde <gauravsanjayshinde@gmail.com>
2023-10-15 17:33:05 -0400

    added TimeWarpLatency & works

--- src/TimeWarpStatistics.cpp
+++ src/TimeWarpStatistics.cpp
@@ -166,21 +166,7 @@ void TimeWarpStatistics::writeToFile(double num_seconds) {
     ofs.close();
 }
 
-template <unsigned I>
-void TimeWarpedLatency::formatLatency(latency_stats_index<I> j, const char* title){
-    auto latency=latency_stats_[j].estimate();
-    // 99, 90 and 50
-    std::cout<<title<<"\n"
-             <<"p99: "<<latency.p99<<"\n"
-             <<"p90: "<<latency.p90<<"\n"
-             <<"p50: "<<latency.p50<<"\n";
-}
-void TimeWarpedLatency::printLatencyStats(){
-    std::cout<<"Latency Stats\n";
-    formatLatency(PROCESS_EVENT_LATENCY,"ProcessEvent");
-    formatLatency(COMPARE_EVENT_LATENCY, "CompareEvent");
-    std::cout<<"\n";
-}
+
 
 void TimeWarpStatistics::printStats() {
 
--- src/TimeWarpStatistics.hpp
+++ src/TimeWarpStatistics.hpp
@@ -5,7 +5,6 @@
 #include <cstdint>  // uint64_t
 #include <tuple>
 #include <string>
-#include "Latency.hpp"
 #include "TimeWarpCommunicationManager.hpp"
 
 namespace warped {
@@ -82,49 +81,7 @@ const stats_index<24> DESIGN_EFFICIENCY;
 const stats_index<25> NUM_STATISTICS;
 
 
-template <unsigned I>
-struct latency_stats_index{
-    latency_stats_index(){}
-    static unsigned const value = I;
-};
 
-struct LatencyStats {
-
-    std::tuple<
-        util::PercentileStats,          //ProcessEvent
-        util::PercentileStats          //CompareEvent
-    > latency_stats_;   
-
-    template<unsigned I>
-    auto operator[](latency_stats_index<I>) -> decltype(std::get<I>(latency_stats_)) {
-        return std::get<I>(latency_stats_);
-    }
-};
-
-const latency_stats_index<0>  PROCESS_EVENT_LATENCY;
-const latency_stats_index<1>  COMPARE_EVENT_LATENCY;
-
-class TimeWarpedLatency{
-public:
-    template <unsigned I>
-    void formatLatency(latency_stats_index<I> j, const char *title);
-
-    LatencyStats latency_stats_;
-
-    void printLatencyStats();
-
-    static TimeWarpedLatency& getInstance(){
-        static TimeWarpedLatency instance;
-        return instance;
-    }
-    
-private:
-    TimeWarpedLatency(){}
-
-public:
-    TimeWarpedLatency(const TimeWarpedLatency& obj)=delete;
-
-};
 
 class TimeWarpStatistics {
 public:
